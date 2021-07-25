cfe61a81e39410e97f4d4f82853867a0d29a45fc
Mariia Grinko <mariiapryashnikova@gmail.com>
2021-07-26 00:28:00 +0300

    Сделала слайдер Doctors, сделала прайс

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('.js-implant-docs').slick({
+
+        infinite: true,
+        // prevArrow: $('.button-prew'),
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+});
