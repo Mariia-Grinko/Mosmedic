ef9b928865dfa07e0fefd635a3d8130e93189b23
Mariia Grinko <mariiapryashnikova@gmail.com>
2021-07-27 00:18:10 +0300

    Сделала блоки с иконками

--- js/script.js
+++ js/script.js
@@ -43,4 +43,20 @@ $(document).ready(function() {
             }
         ]
     });
+    $('.js-implant-example').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
