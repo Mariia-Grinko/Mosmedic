a2f7e1351d1e81f77de59efdf2159003d8f2507d
Mariia Grinko <mariiapryashnikova@gmail.com>
2021-07-26 14:49:34 +0300

    Сделала слайтер Отзывы

--- js/script.js
+++ js/script.js
@@ -21,5 +21,26 @@ $(document).ready(function() {
             }
         ]
     });
+    $('.js-implant-reviews').slick({
 
+        infinite: true,
+        // prevArrow: $('.button-prew'),
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
